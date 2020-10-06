5c80036a78ba60d70c02d19619eb870ecb70258f
torumanabe <manabe@fminc.xsrv.jp>
2020-10-06 10:17:23 +0900

    creat class/functional components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 
-function App() {
-  return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input
-        type="text"
-        onChange={() => {
-          console.log("I am clicked");
-        }}
-      />
-    </React.Fragment>
-  );
-}
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input
+//         type="text"
+//         onChange={() => {
+//           console.log("I am clicked");
+//         }}
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
+  return <div>Hi!</div>;
+};
 
 export default App;
